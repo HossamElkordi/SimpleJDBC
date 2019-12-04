5b76fa95f2635576a5fbb8d400727924d7ea8838
HossamElkordi <hossam_elkordi@yahoo.com>
2019-12-04 14:38:12 +0200

    Lab 4 JDBC

---
+++ SimpleJDBC/src/eg/edu/alexu/csd/oop/jdbc/cs24/GuiClass.java
@@ -0,0 +1,331 @@
+package eg.edu.alexu.csd.oop.jdbc.cs24;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
+
+import eg.edu.alexu.csd.oop.db.cs24.CommandChecker;
+import javax.swing.JTextField;
+
+public class GuiClass {
+
+
+	private JFrame frame;
+	private JTable table;
+	private JTextArea commandArea;
+	private DefaultTableModel model;
+	private JLabel updaredRowslbl;
+	
+	private CommandChecker comCheck;
+	
+	private String document = "";
+	private JButton btnExecuteBatch;
+	private JButton btnClearBatch;
+	private JTextField timeoutField;
+	private JTextField resultRows;
+	private JTextField ColumnControlInput;
+	private JTextField metaInputTxt;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			GuiClass window = new GuiClass();
+			window.frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+			
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GuiClass() {
+		comCheck = new CommandChecker();
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Database Management System");
+		frame.setBounds(100, 100, 632, 730);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JScrollPane tableScrollPane = new JScrollPane();
+		tableScrollPane.setBounds(260, 11, 346, 399);
+		frame.getContentPane().add(tableScrollPane);
+		
+		model = new DefaultTableModel();
+		setTableModel();
+		table = new JTable();
+		table.setModel(model);
+		tableScrollPane.setViewportView(table);
+		
+		setCommand();
+	}
+
+	private void setTableModel() {
+		int count = model.getRowCount();
+		for (int i = 0; i < count; i++) {
+			model.removeRow(0);
+		}
+		String[] colName = comCheck.getColumnsNames();
+		if(colName != null) {
+			model.setColumnIdentifiers(colName);
+		}else {
+			model.setColumnIdentifiers(new String[0]);
+		}
+		Object[][] data = comCheck.getDataSet();
+		if (data != null) {
+			for (int i = 0; i < data.length; i++) {
+				model.addRow(data[i]);
+			} 
+		}
+		
+	}
+
+	private void setCommand() {
+		JScrollPane commandScrollPane = new JScrollPane();
+		commandScrollPane.setBounds(10, 149, 239, 159);
+		frame.getContentPane().add(commandScrollPane);
+		
+		JButton btnExecute = new JButton("Execute");
+		btnExecute.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					int count = document.length();
+					String oldDoc = document;
+					document = commandArea.getText().replaceAll("\n", "");
+					if(document.lastIndexOf(oldDoc, 0) != -1) {
+						comCheck.directCommand(document.substring(count));
+					}else {
+						comCheck.directCommand(document.substring(getCommandStartIndex(oldDoc)));
+					}
+					updaredRowslbl.setText("Number of updated rows: " + comCheck.getUpdatedRows());
+					setTableModel();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnExecute.setBounds(10, 319, 104, 23);
+		frame.getContentPane().add(btnExecute);
+		
+		JButton btnAddToBatch = new JButton("Add to Batch");
+		btnAddToBatch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnAddToBatch.setBounds(146, 319, 104, 23);
+		frame.getContentPane().add(btnAddToBatch);
+		
+		btnExecuteBatch = new JButton("Execute Batch");
+		btnExecuteBatch.setBounds(10, 353, 104, 23);
+		frame.getContentPane().add(btnExecuteBatch);
+		
+		btnClearBatch = new JButton("Clear Batch");
+		btnClearBatch.setBounds(146, 353, 104, 23);
+		frame.getContentPane().add(btnClearBatch);
+		
+		setLabel();
+		setTimeoutStuff();
+		setResultSetStuff();
+		setColumnControl();
+		setMetaDataStuff();
+		setDriverConnectionStatementStuff();
+	}
+	
+	private void setResultSetStuff() {
+		JLabel lblResultsetRows = new JLabel("ResultSet Rows");
+		lblResultsetRows.setHorizontalAlignment(SwingConstants.CENTER);
+		lblResultsetRows.setBounds(62, 492, 118, 14);
+		frame.getContentPane().add(lblResultsetRows);
+		
+		resultRows = new JTextField();
+		resultRows.setBounds(10, 517, 215, 20);
+		frame.getContentPane().add(resultRows);
+		resultRows.setColumns(10);
+		
+		JButton btnAbsolute = new JButton("Set Absolute");
+		btnAbsolute.setBounds(10, 548, 104, 23);
+		frame.getContentPane().add(btnAbsolute);
+		
+		JButton btnNext = new JButton("Next");
+		btnNext.setBounds(136, 548, 89, 23);
+		frame.getContentPane().add(btnNext);
+		
+		JButton btnPrevious = new JButton("Previous");
+		btnPrevious.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnPrevious.setBounds(136, 582, 89, 23);
+		frame.getContentPane().add(btnPrevious);
+		
+		JButton btnBeforePrevious = new JButton("BeforeFr");
+		btnBeforePrevious.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnBeforePrevious.setBounds(10, 582, 104, 23);
+		frame.getContentPane().add(btnBeforePrevious);
+		
+		JButton btnNewButton = new JButton("AfterLast");
+		btnNewButton.setBounds(10, 616, 104, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+	
+	private void setColumnControl() {
+		JLabel lblColumnsControl = new JLabel("Columns Control");
+		lblColumnsControl.setHorizontalAlignment(SwingConstants.CENTER);
+		lblColumnsControl.setBounds(357, 421, 110, 14);
+		frame.getContentPane().add(lblColumnsControl);
+		
+		ColumnControlInput = new JTextField();
+		ColumnControlInput.setBounds(312, 448, 125, 20);
+		frame.getContentPane().add(ColumnControlInput);
+		ColumnControlInput.setColumns(10);
+		
+		JButton getRsultBtn = new JButton("Get Result");
+		getRsultBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		getRsultBtn.setBounds(447, 447, 89, 23);
+		frame.getContentPane().add(getRsultBtn);
+		
+		JLabel lblResult = new JLabel("Result:  ");
+		lblResult.setBounds(312, 483, 294, 14);
+		frame.getContentPane().add(lblResult);
+	}
+	
+	private void setMetaDataStuff() {
+		JLabel lblResultsetMetadata = new JLabel("ResultSet MetaData");
+		lblResultsetMetadata.setHorizontalAlignment(SwingConstants.CENTER);
+		lblResultsetMetadata.setBounds(343, 516, 141, 14);
+		frame.getContentPane().add(lblResultsetMetadata);
+		
+		metaInputTxt = new JTextField();
+		metaInputTxt.setBounds(312, 549, 125, 20);
+		frame.getContentPane().add(metaInputTxt);
+		metaInputTxt.setColumns(10);
+		
+		JButton btnGetColName = new JButton("Get ColName");
+		btnGetColName.setBounds(447, 548, 118, 23);
+		frame.getContentPane().add(btnGetColName);
+		
+		JButton GetTableName = new JButton("Get TableName");
+		GetTableName.setBounds(447, 582, 118, 23);
+		frame.getContentPane().add(GetTableName);
+		
+		JButton btnGetColtype = new JButton("Get colType");
+		btnGetColtype.setBounds(447, 616, 118, 23);
+		frame.getContentPane().add(btnGetColtype);
+		
+		JButton getColCount = new JButton("Get ColCount");
+		getColCount.setBounds(312, 580, 125, 23);
+		frame.getContentPane().add(getColCount);
+		
+		JLabel lblResult_1 = new JLabel("Result: ");
+		lblResult_1.setBounds(312, 657, 253, 14);
+		frame.getContentPane().add(lblResult_1);
+		
+		commandArea = new JTextArea();
+		commandArea.setBounds(10, 151, 237, 157);
+		frame.getContentPane().add(commandArea);
+		commandArea.setTabSize(5);
+		commandArea.setLineWrap(true);
+		commandArea.setFont(new Font("Courier New", Font.PLAIN, 13));
+		commandArea.setWrapStyleWord(true);
+	}
+	
+	private void setDriverConnectionStatementStuff() {
+		JButton btnCreateDriver = new JButton("Create Driver");
+		btnCreateDriver.setBounds(62, 25, 118, 23);
+		frame.getContentPane().add(btnCreateDriver);
+		
+		JLabel lblConnection = new JLabel("Connection:");
+		lblConnection.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConnection.setBounds(10, 68, 65, 23);
+		frame.getContentPane().add(lblConnection);
+		
+		JButton createConBtn = new JButton("Create");
+		createConBtn.setBounds(90, 68, 72, 23);
+		frame.getContentPane().add(createConBtn);
+		
+		JButton closeConBtn = new JButton("Close");
+		closeConBtn.setBounds(169, 68, 72, 23);
+		frame.getContentPane().add(closeConBtn);
+		
+		JLabel lblStatement = new JLabel("Statement:");
+		lblStatement.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStatement.setBounds(10, 102, 65, 23);
+		frame.getContentPane().add(lblStatement);
+		
+		JButton createStmBtn = new JButton("Create");
+		createStmBtn.setBounds(90, 102, 72, 23);
+		frame.getContentPane().add(createStmBtn);
+		
+		JButton closeStmBtn = new JButton("Close");
+		closeStmBtn.setBounds(169, 102, 72, 23);
+		frame.getContentPane().add(closeStmBtn);
+	}
+	
+	private void setTimeoutStuff() {
+		timeoutField = new JTextField();
+		timeoutField.setBounds(10, 448, 86, 20);
+		frame.getContentPane().add(timeoutField);
+		timeoutField.setColumns(10);
+		
+		JButton btnSet = new JButton("Set");
+		btnSet.setBounds(106, 447, 56, 23);
+		frame.getContentPane().add(btnSet);
+		
+		JButton btnGet = new JButton("Get");
+		btnGet.setBounds(169, 447, 56, 23);
+		frame.getContentPane().add(btnGet);
+		
+		JLabel lblTimeoutzeroFor = new JLabel("Timeout (zero for default)");
+		lblTimeoutzeroFor.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTimeoutzeroFor.setBounds(62, 423, 132, 14);
+		frame.getContentPane().add(lblTimeoutzeroFor);
+	}
+
+	private void setLabel() {
+		updaredRowslbl = new JLabel("Number of updated rows: 0");
+		updaredRowslbl.setHorizontalAlignment(SwingConstants.CENTER);
+		updaredRowslbl.setBounds(0, 387, 240, 23);
+		frame.getContentPane().add(updaredRowslbl);
+	}
+	
+	private int getCommandStartIndex(String oldDoc) {
+		int start = 0;
+		for (int i = 0; i < document.length(); i++) {
+			if(oldDoc.charAt(i) != document.charAt(i)) {
+				start = i;
+				break;
+			}
+		}
+		return start;
+	}
+}
